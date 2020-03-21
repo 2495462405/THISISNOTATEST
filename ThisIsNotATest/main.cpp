bc2f900f3f6f39f79232425d0ee29864c169b80e
xuyishen <xzj2495462405@outlook.com>
2020-03-21 09:12:35 +0800

    Initial Commit

---
+++ ThisIsNotATest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ThisIsNotATest
+//
+//  Created by 徐亦燊 on 2020/3/21.
+//  Copyright © 2020 徐亦燊. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
